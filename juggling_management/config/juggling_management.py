97b0d77de5cc3eea53d6c42cc6dde429078ab7eb
Robin Rosenstock <robin.rosenstock@t-online.de>
2020-12-28 14:48:27 +0100

    this works

--- juggling_management/config/juggling_management.py
+++ juggling_management/config/juggling_management.py
@@ -5,29 +5,28 @@ def get_data():
     return [
       {
         "label":_("Juggling Management"),
-        "icon": "octicon octicon-briefcase",
         "items": [
                                    {
               "type": "doctype",
-              "name": "jugglingtrick_juggling_management",
+              "name": "Jugglingtrick_juggling_management",
               "label": _("Jugglingtrick"),
               "description": _("Description of Jugglingtricks"),
             },
                     {
               "type": "doctype",
-              "name": "routine_juggling_management",
+              "name": "Routine_juggling_management",
               "label": _("Routine"),
               "description": _("Description of Lists"),
             },
                                    {
               "type": "doctype",
-              "name": "category_juggling_management",
+              "name": "Category_juggling_management",
               "label": _("Category"),
               "description": _("Description of Categories"),
             },
                                    {
               "type": "doctype",
-              "name": "prop_juggling_management",
+              "name": "Prop_juggling_management",
               "label": _("Prop"),
               "description": _("Description of Props"),
             }
